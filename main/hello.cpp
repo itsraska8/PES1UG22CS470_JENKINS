b08f612275bfb932ffc88a02d567854adb70ddcb
itsraska8 <148065897+itsraska8@users.noreply.github.com>
2025-03-21 10:24:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,10 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << R"( 
+    (\(\ 
+    ( -.-) 
+    o__(")(") 
+)" << endl;
   return 0;
 }
